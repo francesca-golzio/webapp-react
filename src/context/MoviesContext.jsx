4427b350c2b07474a109d62419ed09168d8bed4d
Francesca Golzio <francescagolzio@gmail.com>
2026-04-22 17:12:40 +0200

    debug context to prevent infinite loop

--- src/context/MoviesContext.jsx
+++ src/context/MoviesContext.jsx
@@ -1,5 +1,4 @@
-import { createContext } from "react";
-import { useState, useEffect } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
 const MoviesContext = createContext();
 const movieListAPI = import.meta.env.VITE_MOVIE_LIST_API
@@ -7,26 +6,27 @@ const movieListAPI = import.meta.env.VITE_MOVIE_LIST_API
 function MoviesProvider({ children }) {
   /* qui quello che vogliamo condividere */
   const [movies, setMovies] = useState([]);
-  fetch(movieListAPI)
-    .then(res => res.json())
-    .then(data => {
-      setMovies(...movies, data);
-      return movies;
-    })
+
+  useEffect(() => {
+    fetch(movieListAPI)
+      .then(res => res.json())
+      .then(data => setMovies(data))
+      .catch(err => console.log(err))
+  }, []);
 
   return (
-    <MoviesProvider
-      value={{
-        /* qui i valori da condividere */
-        movies
-      }}>
-
-    </MoviesProvider>
+    <MoviesContext.Provider
+      value={{ movies }}>
+      {children}
+    </MoviesContext.Provider>
   )
 }
 
 function useMovies() {
   const context = useContext(MoviesContext);
+  if(!context) {
+    console.log('loading');    
+  }
   return context;
 }
 
