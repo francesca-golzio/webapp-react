7d9dffcbdb9100ad3f00a05455b96271cc72cca8
Francesca Golzio <francescagolzio@gmail.com>
2026-04-22 09:59:23 +0200

    fix home
    - fix typos
    - delete misplaced properties

--- src/pages/MoviesHome.jsx
+++ src/pages/MoviesHome.jsx
@@ -6,9 +6,9 @@ export default function MoviesHome() {
     <>
       {/* Jumbotron */}
       <div className="container-fluid p-5 bg-warning">
-        <h2 className="display-5 fw-bold">Wellcome on Moovies Reviews!</h2>
-        <p className="col-md-8 fs-4 text-muted">
-          This is a simple React app to review movies. You can find the list of movies below in the page and click on each movie to see its details and reviews. On the moovie details page you can also add your own review.
+        <h2 className="display-5 fw-bold">Welcome to Movies Reviews!</h2>
+        <p className="fs-4 text-muted">
+          This is a simple React app to review movies. You can find the list of movies below in the page and click on each movie to see its details and reviews. On the movie details page you can also add your own review.
         </p>
         <h3>What are you waiting for? <br />Go and share the <i className="bi bi-heart-fill"></i> for movies!</h3>
       </div>
