9015c1f2a341f9a83db8f0c693c51907a8d4e5d6
Francesca Golzio <francescagolzio@gmail.com>
2026-04-25 13:33:02 +0200

    reinstate `MoviesListCard` component

--- src/components/MoviesListCard.jsx
+++ src/components/MoviesListCard.jsx
@@ -1,4 +1,4 @@
-//⚠️ component temporary dismissed
+
 
 /* import context */
 import { useMovies } from "../context/MoviesContext";
@@ -9,16 +9,12 @@ export default function MoviesListCard(movie) {
 
   return (
     movies.map(movie => {
-      /* destructuring params */
       const { id, title, genre, image, created_at, updated_at } = movie;
-
       return (
-        <div className="card col-sm-12 col-md-4 col-lg-3 m-3 px-0" key={id}>
+        <div className="card col-sm-12 col-md-4 col-lg-3 m-3 px-0" key={id + (Date.toString)}>
           <img className="card-img-top" src={image} />
           <div className="card-body">
             <div className="card-title">{title}</div>
-            <div></div>
-            {/* {updated_at ? <div>{updated_at}</div> : <div>{created_at}</div>} */}
           </div>
         </div>
       )
--- src/components/MoviesListCardDeck.jsx
+++ src/components/MoviesListCardDeck.jsx
@@ -1,7 +1,7 @@
 /* import context */
 import { useMovies } from "../context/MoviesContext";
 /* import components */
-//import MoviesListCard from "./MoviesListCard";
+import MoviesListCard from "./MoviesListCard";
 
 export default function MoviesListCardDeck() {
 
@@ -13,19 +13,7 @@ export default function MoviesListCardDeck() {
       <div className="row justify-content-evenly my-3">
         {
           /* cycle inside the movies array */
-          movies.map(movie => {
-            const { id, title, genre, image, created_at, updated_at } = movie;
-            return (
-              <div className="card col-sm-12 col-md-4 col-lg-3 m-3 px-0" key={id + (Date.toString)}>
-                <img className="card-img-top" src={image} />
-                <div className="card-body">
-                  <div className="card-title">{title}</div>
-                  <div></div>
-                  {/* {updated_at ? <div>{updated_at}</div> : <div>{created_at}</div>} */}
-                </div>
-              </div>
-            )
-          })
+          <MoviesListCard />
         }
       </div>
     </div>
